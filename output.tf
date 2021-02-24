f8b9ef863d173f28678d6330fe2b9a0a49024bb7
Robert de Bock <robert@meinit.nl>
2021-02-24 15:51:52 +0100

    Use the generic name in the output as well.

--- output.tf
+++ output.tf
@@ -1,34 +1,34 @@
 output "id" {
   description = "The virtual NetworkConfiguration ID."
-  value       = azurerm_virtual_network.vnet.id
+  value       = azurerm_virtual_network.default.id
 }
 
 output "name" {
   description = "The name of the virtual network."
-  value       = azurerm_virtual_network.vnet.name
+  value       = azurerm_virtual_network.default.name
 }
 
 output "resource_group_name" {
   description = "The name of the resource group in which to create the virtual network."
-  value        = azurerm_virtual_network.vnet.resource_group_name
+  value        = azurerm_virtual_network.default.resource_group_name
 }
 
 output "location" {
   description = "The location/region where the virtual network is created."
-  value       = "azurerm_virtual_network.vnet.location"
+  value       = azurerm_virtual_network.default.location
 }
 
 output "address_space" {
   description = "The list of address spaces used by the virtual network."
-  value       = azurerm_virtual_network.vnet.address_space
+  value       = azurerm_virtual_network.default.address_space
 }
 
 output "guid" {
   description = "The GUID of the virtual network."
-  value       = azurerm_virtual_network.vnet.guid
+  value       = azurerm_virtual_network.default.guid
 }
 
 output "subnet" {
   description = "One or more subnet blocks as defined below."
-  value       = azurerm_virtual_network.vnet.subnet
+  value       = azurerm_virtual_network.default.subnet
 }
